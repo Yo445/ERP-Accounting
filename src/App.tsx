f118fcc5cfff96f164e9b9c6d0797449ecd14dd7
Yo445 <ya10894@gmail.com>
2024-11-11 16:54:15 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Landing from './components/Landing page components/Landing';
+
+function App() {
+  return (
+<Landing />
+  );
+}
+
+export default App;
